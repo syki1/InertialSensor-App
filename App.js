9734ff18e0e2abceffb96a5c3ebaa7bde6cc3449
syki1 <syki10@gmail.com>
2020-08-19 17:37:18 +0200

    Add two buttons on first screen

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Apka działa jeee !!!</Text>
+      <Text>Inertial sensor App</Text>
       <StatusBar style="auto" />
+    
+    
+    <Button style={styles.button}           
+      title="O Aplikacji"
+      type="a"
+      >
+      </Button> 
+      <Button style={styles.button}           
+      title="Nowa gra"
+      type="a"
+      ></Button>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontSize: "24px",
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#cecece',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    padding: "10px",
+    color: "gray",
+    backgroundColor: "red",
+    width: "200px"
+  }
 });
